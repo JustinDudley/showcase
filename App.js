da8ac8d530ffcc19977e8cd80a535ebbdf0ae38b
justindudley <justin.dudley@ascendum.com>
2020-09-06 00:29:02 -0400

    successful navigation to initial screen of Home. Using yarn ios to run

--- App.js
+++ App.js
@@ -4,13 +4,18 @@ import * as React from 'react';
 import {View, Text} from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import {Home} from './routes/home.js';
+
+const Stack = createStackNavigator();
 
 const App: () => React$Node = () => {
   return (
     <NavigationContainer>
-      <View>
-        <Text>Hello</Text>
-      </View>
+      <Stack.Navigator initialRouteName="home">
+        <Stack.Screen name="home" component={Home} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
--- routes/home.js
+++ routes/home.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 import {View, Text, StyleSheet} from 'react-native';
 
-const Home: React.FC = () => {
-  return (
-    <View>
-      <Text style={styles.home}>Hello from Home</Text>
-    </View>
-  );
-};
+export class Home extends React.Component {
+  render() {
+    // const Home: React.FC = () => {
+    return (
+      <View>
+        <Text style={styles.home}>Hello from Home</Text>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   home: {
